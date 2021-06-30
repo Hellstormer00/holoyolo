d785494ce5117a9da03cab57e158fde99f21d5fe
alex <alexander.haas00@web.de>
2021-06-30 16:39:39 +0200

    server yolo

--- server/server.py
+++ server/server.py
@@ -1,9 +1,11 @@
+from server.detection import get_output, init_net, tidy_output
 import socket
 import cv2 as cv
 import numpy as np
+from detection import *
 
 host, port = '172.22.223.57', 9001
-
+MIN_CONF = 0.9
 
 def recv_all(size, conn):
     buf = b""
@@ -30,23 +32,34 @@ def recv_img(conn):
     return img
 
 
+def init_socket(host, port, s):
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind((host, port))
+    s.listen()
+    print("server initialized")
+    print("listening on port", port)
+
+
 if __name__ == "__main__":
 
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        s.bind((host, port))
-        s.listen()
+    net, ln = init_net()
 
-        print("server initialized")
-        print("listening on port", port)
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        init_socket(host, port, s)
 
         while True:
             conn, addr = s.accept()
             print(f"connected to {addr}")
 
             with conn:
-                recv_img(conn)
+                img = recv_img(conn)
                 img = img_processing(img)
+                
+                net_outputs = get_output(img, net, ln)
+                pred = tidy_output(net_outputs, MIN_CONF)
+
+                print(pred)
+
                 cv.imshow("Yaaaay", img)
                 cv.waitKey(0)
 
