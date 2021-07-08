e8d7d2f04676e40239a5f1b45cdf88e25e85a50d
alex <alexander.haas00@web.de>
2021-07-08 13:41:39 +0200

    timing

--- client/client.py
+++ client/client.py
@@ -1,11 +1,23 @@
 import socket
 import numpy as np
 import cv2 as cv
+import time
+import numpy as np
 
 host, port = "0.0.0.0", 9003
 DEL = b",\t"
 EOM = b"\x04"
 
+def timer(func):
+    def wrapper(name):
+        t1 = time.time()
+        out = func(name)
+        t2 = time.time()
+        print("Timed: {:.2f} s".format(t2 - t1))
+        return out
+    return wrapper
+
+@timer
 def send_pic(img_name):
     img = cv.imread(img_name)
     img = cv.imencode(".png", img)[1].tostring()
@@ -25,7 +37,10 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Horse:\n" + send_pic("horse.jpg"))
     print("Dog:\n" + send_pic("dog.jpg"))
 
+    while True:
+       print("Dog:\n" + send_pic("dog.jpg")) 
+
     s.sendall(b"STOP")
 
-
+d
 
